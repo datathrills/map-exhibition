63385d26ed3231ada0e593dab65ddca05c330db0
Marko Bralic <datathrills@gmail.com>
2019-08-11 06:55:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const fs = require('fs');
+
+const contents = fs.readFileSync("data/data.json");
+const dataContents = JSON.parse(contents);
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    //res.sendFile(__dirname + '/index.html');
+    res.render('map', {data: dataContents})
+});
+
+app.get('/projector', function(req, res){
+    res.sendFile(__dirname + '/projector.html');
+})
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('data', function(msg){
+        console.log('message: ' + msg);
+        io.emit('message', msg);
+    });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
